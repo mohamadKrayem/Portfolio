0e730d01ffa353fccd63d60eb6d8b30078b388ce
Mohamad Krayem <mk2242682@gmail.com>
2022-08-17 17:43:48 +0300

    editing menu button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
       <h2 className='text-xl font-bold lg:basis-2/5 md:basis1/5 mt-6 md:mt-0' >Mohamad</h2>
     
-      <button onClick={onClick} data-collapse-toggle="navbar-default" type="button" className="fixed right-8 top-4 z-30 button inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+      <button onClick={onClick} data-collapse-toggle="navbar-default" type="button" className="fixed right-8 top-4 z-30 button inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-amber-400 outline-none ring-2 ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
       </button>
