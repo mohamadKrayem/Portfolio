2975a8da835e8dcbd6ef485c711ec625b2ebfbe8
Mohamad Krayem <82458498+mohamadKrayem@users.noreply.github.com>
2023-04-15 10:30:09 +0000

    add links and requestcli project

--- src/Section/Footer.jsx
+++ src/Section/Footer.jsx
@@ -5,52 +5,65 @@ import { ImFacebook } from "react-icons/im";
 
 const Footer = () => {
   return (
-    <footer className='col-center mt-16 md:w-auto w-80'>
+    <footer className="col-center mt-16 md:w-auto w-80">
       <div className="flex flex-wrap justify-center gap-12 w-[90%]">
-        <div className='lg:basis-1/3 w-full md:w-auto text-start'>
-          <h1 className='footer-title'>About</h1>
-          <p className='font-["Poppins"] text-xl text-skin-fourth leading-9 mb-6'>I'm a Lebanese student. Writing readable, reusable, clean code is my goal in my work, helping people is my passion in life. My first rule is that getting to know someone new is never a loss.</p>
+        <div className="lg:basis-1/3 w-full md:w-auto text-start">
+          <h1 className="footer-title">About</h1>
+          <p className='font-["Poppins"] text-xl text-skin-fourth leading-9 mb-6'>
+            I'm a Lebanese student. Writing readable, reusable, clean code is my
+            goal in my work, helping people is my passion in life. My first rule
+            is that getting to know someone new is never a loss.
+          </p>
           <div className="flex items-center slef-start gap-3">
             <div className="media">
-              <BsInstagram className="text-2xl text-skin-fourth "/>
+              <a href="https://www.instagram.com/mo__krayem">
+                <BsInstagram className="text-2xl text-skin-fourth " />
+              </a>
             </div>
             <div className="media">
-              <BsTwitter className="text-2xl text-skin-fourth "/>
+              <a href="https://twitter.com/mo_krayem">
+              <BsTwitter className="text-2xl text-skin-fourth " />
+              </a>
             </div>
             <div className="media">
-              <ImFacebook className="text-2xl text-skin-fourth "/>
+              <a href="https://www.facebook.com/profile.php?id=100080223470282">
+              <ImFacebook className="text-2xl text-skin-fourth " />
+              </a>
             </div>
           </div>
         </div>
 
-        <div className='lg:basis-1/5 w-full md:w-auto text-start'>
-          <h1 className='footer-title'>Services</h1>
-          <li className='service'>Web development</li>
-          <li className='service'>front-end</li>
-          <li className='service'>back-end</li>
-          <li className='service'>full-stack</li>
-          <li className='service'>Ux/Ui desing</li>
+        <div className="lg:basis-1/5 w-full md:w-auto text-start">
+          <h1 className="footer-title">Services</h1>
+          <li className="service">Web development</li>
+          <li className="service">front-end</li>
+          <li className="service">back-end</li>
+          <li className="service">full-stack</li>
+          <li className="service">Ux/Ui desing</li>
         </div>
 
-        <div className='lg:basis-1/3 w-full md:w-auto text-start'>
-          <h1 className='footer-title'>Have a question?</h1>
+        <div className="lg:basis-1/3 w-full md:w-auto text-start">
+          <h1 className="footer-title">Have a question?</h1>
           <div className="flex items-center gap-2">
-            <HiPhone className="text-xl text-skin-fourth"/>
-            <p className='font-["Poppins"] text-xl text-skin-fourth mb-2'>+961 3 639 845</p>
+            <HiPhone className="text-xl text-skin-fourth" />
+            <p className='font-["Poppins"] text-xl text-skin-fourth mb-2'>
+              +961 3 639 845
+            </p>
           </div>
           <div className="flex items-center gap-2">
-            <SiMinutemailer className="text-xl text-skin-fourth"/>
+            <SiMinutemailer className="text-xl text-skin-fourth" />
             <p className='font-["Poppins"] text-xl text-skin-fourth'>
-              mohamadkrayem@yandex.com
+              mohamadkrayem@email.com
             </p>
           </div>
         </div>
       </div>
 
-      <p className="md:text-2xl sm:text-xl  my-10 text-skin-fourth">Copyright &copy; 2022 all rights reserved</p>
-      
+      <p className="md:text-2xl sm:text-xl  my-10 text-skin-fourth">
+        Copyright &copy; 2022 all rights reserved
+      </p>
     </footer>
-  )
+  );
 }
 
 export default Footer
--- src/Section/Projects.jsx
+++ src/Section/Projects.jsx
@@ -4,17 +4,49 @@ import Project from '../components/Project'
 const Projects = () => {
 
   return (
-    <div className='col-center md:h-screen md:mb-12 mb-10' id='projects'>
-      <h1 className='font-bold text-5xl leading-normal w-80 mb-6 text-center font-["Poppins"] md:mb-12 mb-10 text-skin-secondary'>My Projects</h1>
-      <div className='row-center flex-wrap gap-5 w-4/5'>
-        <Project title="Calculator" url="/assets/CalculatorApp.jpg" repo="https://github.com/mohamadkrayem/calculator" demo="https://mocalculatorapp.netlify.app/"/>
-        <Project title="Advice generator app" url="/assets/AdviceApp.png" repo="https://github.com/mohamadkrayem/Advice-Generator-App" demo="https://advice-generator-app-delta-five.vercel.app/"/>
-        <Project title="Huddle Challenge" url="/assets/Huddle.png" repo="https://github.com/mohamadKrayem/Huddle-Challenge" demo="https://huddle-challenge-five.vercel.app/"/>
-        <Project title="Scandiweb" url="/assets/Scandiweb.png" repo="https://github.com/mohamadkrayem/scandiweb-test" demo="https://react-frontend-jade.vercel.app/"/>
-        <Project title="Github search app" url="/assets/githubSearchApp.png" repo="https://github.com/mohamadkrayem/GithubSearchApp-React" demo="https://react-r97ktp.stackblitz.io/"/>
+    <div className="col-center md:h-screen md:mb-12 mb-10" id="projects">
+      <h1 className='font-bold text-5xl leading-normal w-80 mb-6 text-center font-["Poppins"] md:mb-12 mb-10 text-skin-secondary'>
+        My Projects
+      </h1>
+      <div className="row-center flex-wrap gap-5 w-4/5">
+        <Project
+          title="Calculator"
+          url="/assets/CalculatorApp.jpg"
+          repo="https://github.com/mohamadkrayem/calculator"
+          demo="https://mocalculatorapp.netlify.app/"
+        />
+        <Project
+          title="Advice generator app"
+          url="/assets/AdviceApp.png"
+          repo="https://github.com/mohamadkrayem/Advice-Generator-App"
+          demo="https://advice-generator-app-delta-five.vercel.app/"
+        />
+        <Project
+          title="Huddle Challenge"
+          url="/assets/Huddle.png"
+          repo="https://github.com/mohamadKrayem/Huddle-Challenge"
+          demo="https://huddle-challenge-five.vercel.app/"
+        />
+        <Project
+          title="Scandiweb"
+          url="/assets/Scandiweb.png"
+          repo="https://github.com/mohamadkrayem/scandiweb-test"
+          demo="https://react-frontend-jade.vercel.app/"
+        />
+        <Project
+          title="Github search app"
+          url="/assets/githubSearchApp.png"
+          repo="https://github.com/mohamadkrayem/GithubSearchApp-React"
+          demo="https://react-r97ktp.stackblitz.io/"
+        />
+        <Project 
+          title="RequestCLI"
+          url="/assets/requestcli.png"
+          repo="https://github.com/mohamadkrayem/requestcli"
+        />
       </div>
     </div>
-  )
+  );
 }
 
 export default Projects;
